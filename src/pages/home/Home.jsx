3a49925307eb8d4fd428f6643e686f28e7cf4792
mario-cartagena <jhonmariocartagena@gmail.com>
2023-05-23 21:51:24 -0500

    add configure new repository

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Home.scss";
+export const Home = () => {
+    return (
+        <div className="home">
+            <h1 className="home-tit">HOME</h1>
+        </div>
+    );
+}
---
+++ src/pages/technology/Technology.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TechnologyComponent } from '../../components/technology/TechnologyComponent'
+import './StyleTechnology.scss'
+
+export const Technology = () => {
+  return (
+    <div className='technology'>
+      <TechnologyComponent/>
+    </div>
+  )
+}
