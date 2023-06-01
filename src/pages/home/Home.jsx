fa0e3004323f50231f09592f2bf49b7323a1c39f
mario-cartagena <jhonmariocartagena@gmail.com>
2023-06-01 00:21:55 -0500

    componente home con responsive y menu hamburguesa

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
 export const Home = () => {
+
     return (
-        <div className="home">
-            <h1 className="home-tit">HOME</h1>
-        </div>
-    );
+        
+        <section className="container home">
+            <div className="home__wrap">
+                <div className="home__left">
+                    <h5>So, you want to travel to</h5>
+                    <h1>Space</h1>
+                    <p>
+                        Let’s face it; if you want to go to space, you might as
+                        well genuinely go to outer space and not hover kind of
+                        on the edge of it. Well sit back, and relax because
+                        we’ll give you a truly out of this world experience!
+                    </p>
+                </div>
+                <h4 className="home__right">
+                    <a
+                        href="https://earth.google.com/web/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Explore
+                    </a>
+                    <div className="circle__below"></div>
+                </h4>
+            </div>
+        </section>
+    )
 }
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/home/Home';
 import { Crew } from '../pages/crew/Crew';
 import { Destination }  from '../pages/destination/Destination';
@@ -14,7 +14,7 @@ const AppRouter = () => {
                 <Routes>
                     <Route element={<Layout/>}>
                         <Route index element={<Home/>}/>
-                        <Route path={"home"} element={<Home/>}/>
+                        <Route path={"home"} element={<Navigate to="/"/>}/>
                         <Route path={"destination"} element={<Destination/>}/>
                         <Route path={"crew"} element={<Crew/>}/>
                         <Route path={"technology"} element={<Technology/>}/>
